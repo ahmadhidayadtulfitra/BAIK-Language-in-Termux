8d31f70c328dbdef6a8f897d12f7977b399c0187
ahmadhidayadtulfitra <84166294+ahmadhidayadtulfitra@users.noreply.github.com>
2021-06-15 09:32:54 +0800

    Delete Baik-language directory

--- Baik-language/compile-simple.sh
+++
@@ -1,4 +0,0 @@
-
-clang -o baik -DUSE_GD -DUSE_CGI -DUSE_WEBSTYLE -DUSE_SQLITE -DLINUX -I/usr/include -I/usr/local/include -L/usr/lib -L/usr/lib64 -L/usr/local/lib  tbaik.c baik_ident.c baik_stack.c baik_expression.c baik_compare.c baik_factor.c interpreter.c interpreterSub.c interpreterClass.c sqlite_common.c pq_common.c -lpthread -lm  -lsqlite3 -lgd 
-
-
